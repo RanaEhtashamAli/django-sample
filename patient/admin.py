e90dd820bf6c2bfa95e0d67f93b0aac775168e29
ranaehtashamali <ranaehtashamali1@gmail.com>
2023-07-05 11:50:19 +0500

    Removed files

--- patient/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Patient, PatientVisit, PatientPrescription
-
-admin.site.register(Patient)
-admin.site.register(PatientVisit)
-admin.site.register(PatientPrescription)
