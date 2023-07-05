2b878923a6f7b202a128d3780e7b9d7e61bc0c04
ranaehtashamali <ranaehtashamali1@gmail.com>
2023-07-05 11:52:26 +0500

    Removed files

--- doctor/urls.py
+++
@@ -1,7 +0,0 @@
-from .views import DoctorViewSet
-from rest_framework.routers import DefaultRouter
-
-router = DefaultRouter()
-router.register(r'doctors', DoctorViewSet, basename='doctor')
-urlpatterns = router.urls
-
